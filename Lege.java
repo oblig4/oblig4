148623ebf56759f567c4b94a8ebd20bfb4b7af2c
melinda-s <61828994+melinda-s@users.noreply.github.com>
2020-03-06 14:18:09 +0100

    Update Lege.java

--- Lege.java
+++ Lege.java
@@ -1,8 +1,10 @@
-class Lege {
+class Lege implements Comparable<Lege> {
   protected String legensNavn;
+  protected Lenkeliste<Resept> utskrevedeResepter;
 
   public Lege(String legensNavn) {
     this.legensNavn = legensNavn;
+    utskrevedeResepter = new Lenkeliste<Resept>();
   }
 
   public String hentNavn() {
@@ -12,5 +14,58 @@ class Lege {
   @Override
   public String toString() {
     return "Legens navn: " + legensNavn;
+  }
+
+  @Override
+  public int compareTo(Lege other) {
+    return legensNavn.compareTo(other.hentNavn());
+  }
+
+  public Lenkeliste<Resept> hentReseptliste() {
+    return utskrevedeResepter;
+  }
+
+  public HvitResept skrivHvitResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
+    if (legemiddel instanceof Narkotisk) {
+      throw new UlovligUtskrift(this, legemiddel);
+    }
+
+    HvitResept nyResept = new HvitResept(legemiddel, this, pasient, reit);
+    utskrevedeResepter.leggTil(nyResept);
+
+    return nyResept;
+  }
+
+  public MilitaerResept skrivMilitaerResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
+    if (legemiddel instanceof Narkotisk) {
+      throw new UlovligUtskrift(this, legemiddel);
+    }
+
+    MilitaerResept nyResept = new MilitaerResept(legemiddel, this, pasient, reit);
+    utskrevedeResepter.leggTil(nyResept);
+
+    return nyResept;
+  }
+
+  public PResept skrivPResept(Legemiddel legemiddel, Pasient pasient) throws UlovligUtskrift {
+    if (legemiddel instanceof Narkotisk) {
+      throw new UlovligUtskrift(this, legemiddel);
+    }
+
+    PResept nyResept = new PResept(legemiddel, this, pasient);
+    utskrevedeResepter.leggTil(nyResept);
+
+    return nyResept;
+  }
+
+  public BlaaResept skrivBlaaResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
+    if (legemiddel instanceof Narkotisk) {
+      throw new UlovligUtskrift(this, legemiddel);
+    }
+
+    BlaaResept nyResept = new BlaaResept(legemiddel, this, pasient, reit);
+    utskrevedeResepter.leggTil(nyResept);
+
+    return nyResept;
   }
 }
