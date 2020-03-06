8bbf47ea2d297c575025d9410811e1a6ee550337
maryahai <61828825+maryahai@users.noreply.github.com>
2020-03-06 13:18:36 +0100

    Update Pasient.java

--- Pasient.java
+++ Pasient.java
@@ -1,3 +1,29 @@
-public class Pasient {
- 
+class Pasient{
+    private String PasientNavn;
+    private String foedselnummer;
+
+    static int pasientid = 0;
+    Stabel<Resept> resept = new Stabel<>();
+
+    public Pasient(String pn, String fnr){
+        PasientNavn = pn;
+        foedselnummer = fnr;
+
+        pasientid ++;
+    }
+
+    void leggTilResept(Resept nyResept)
+    {
+        this.resept.leggPaa(nyResept);
+    }
+
+    public String hentPasientNavn(){
+        return PasientNavn;
+    }
+    public String hentFoedselnummer(){
+        return foedselnummer;
+    }
+    public int hentPasientId(){
+        return pasientid;
+    }
 }
