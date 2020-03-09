ae83971bc9c02ff8920ce624b2586bccec4663d9
melinda-s <61828994+melinda-s@users.noreply.github.com>
2020-03-09 16:55:26 +0100

    Update BlaaResept.java

--- BlaaResept.java
+++ BlaaResept.java
@@ -1,7 +1,7 @@
 class BlaaResept extends Resept {
 
-  public BlaaResept(Legemiddel legemiddel, Lege utskrivendeLege, int pasientId, int reit) {
-    super(legemiddel, utskrivendeLege, pasientId, reit);
+  public BlaaResept(Legemiddel legemiddel, Lege utskrivendeLege, Pasient pasient, int reit) {
+    super(legemiddel, utskrivendeLege, pasient, reit);
   }
 
   @Override
